7ae12c19028b422ef593f4c1c5d7f39d0738fa12
Ben Aguirre <ben.aguirre03@gmail.com>
2023-02-16 18:05:17 -0600

    Update Styling 	- Added container and ID's change for btn

--- main.js
+++ main.js
@@ -11,14 +11,14 @@ const gameBoard = (() => {
         'x'
     ]
 
-    let displayBtn = document.querySelector('#display')
+    let displayBtn = document.querySelector('#display-btn')
     displayBtn.addEventListener('click', () => {
         displayGameBoard();
     })
 
     const displayGameBoard = () => {
         for (i = 0; i <= gameBoardObject.length; i++) {
-            let container = document.querySelector('.container')
+            let container = document.querySelector('.game-board')
             let div = document.createElement('div')
             container.appendChild(div)
             div.textContent = [gameBoardObject[i]]
