a56aed2cf807b069565bea35d1c381b125a362a8
Ben Aguirre <ben.aguirre03@gmail.com>
2023-02-17 14:22:57 -0600

    Add Data-set and box event listener 	- Event listener adds data-set number to div

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const gameBoard = (() => {
     let gameBoardObject = [
         '', 
-        'x', 
-        'x', 
-        'o', 
-        'o',
-        'o',
-        'x',
-        'x',
-        'x'
+        '', 
+        '', 
+        '', 
+        '',
+        '',
+        '',
+        '',
+        '',
     ]
 
     let displayBtn = document.querySelector('#display-btn')
@@ -17,15 +17,12 @@ const gameBoard = (() => {
     })
 
     const displayGameBoard = () => {
-        for (i = 0; i <= gameBoardObject.length; i++) {
+        for (i = 0; i < gameBoardObject.length; i++) {
             let container = document.querySelector('.game-board')
             let div = document.createElement('div')
+            div.dataset.boxNumber = i;
             div.classList.add('box')
             container.appendChild(div)
-            div.textContent = [gameBoardObject[i]]
-            div.addEventListener('click', () => {
-                // console.log('test')
-            })
         }
     }
     return {
@@ -49,7 +46,7 @@ const playGame = (() => {
     console.log(board)
     board.forEach(div => {
         div.addEventListener('click', () => {
-            div.textContent = 'TEST'
+            div.textContent = div.dataset.boxNumber
         })
     })
 })();
