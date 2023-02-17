8f56e1ba1a60fd1fdbfbaefa57dd0e5716d17a33
Ben Aguirre <ben.aguirre03@gmail.com>
2023-02-16 18:03:23 -0600

    Players Fact function add

--- main.js
+++ main.js
@@ -32,5 +32,12 @@ const gameBoard = (() => {
     }
 })();
 
-
+const Player = (name, symbol) => {
+    const getName = () => name
+    const getSymbol = () => symbol
+    return {
+        getName, 
+        getSymbol
+    }
+}
 
