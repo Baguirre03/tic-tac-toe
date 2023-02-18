a06c7b9badf60df09e128cbfa2bcadf404ead337
Ben Aguirre <ben.aguirre03@gmail.com>
2023-02-18 13:33:24 -0600

    HTML changes for player 	- Also Main.js changes to create players based on inputs

--- main.js
+++ main.js
@@ -25,11 +25,17 @@ const Players = (name, symbol) => {
 }
 
 const playGame = (() => {
-    let player1 = Players('player1', 'x')
-    let player2 = Players('player2', 'y')
     let boardContainer = document.querySelector('.game-board')
+    let clearGameBtn = document.querySelector('.restart-btn')
+    let playerOneName = document.querySelector('#player1').value
+    let playerTwoName = document.querySelector('#player2').value
+    let playerOneSym = document.querySelector('#symbol1').value
+    let playerTwoSym = document.querySelector('#symbol1').value
 
     const displayGameBoard = () => {
+        let player1 = Players(playerOneName, playerOneSym)
+        let player2 = Players(playerTwoName, playerTwoSym)
+
         for (i = 0; i < gameBoard.gameBoardObject.length; i++) {
             let div = document.createElement('div')
             div.dataset.boxNumber = i;
@@ -45,15 +51,19 @@ const playGame = (() => {
 
     let displayBtn = document.querySelector('#display-btn')
     displayBtn.addEventListener('click', () => {
+        clearGameBoardObject();
         clearGameBoard();
         displayGameBoard();
-        displayBtn.textContent = 'Restart Game'
     })
 
     const clearGameBoard = () => {
         while (boardContainer.firstChild) {
             boardContainer.removeChild(boardContainer.firstChild)
         }
+    }
+
+    const clearGameBoardObject = () => {
+        gameBoard.gameBoardObject = ['', '', '', '', '','','','','',]
     }
     
     const placeSpot = (player, spot) => {
