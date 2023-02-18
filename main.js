a6f0327c957e8525d7a1900b0223b4797e64246a
Ben Aguirre <ben.aguirre03@gmail.com>
2023-02-18 13:18:55 -0600

    Updated gameBoardObject when div is clicked

--- main.js
+++ main.js
@@ -29,28 +29,37 @@ const playGame = (() => {
     let player2 = Players('player2', 'y')
     let boardContainer = document.querySelector('.game-board')
 
-    let displayBtn = document.querySelector('#display-btn')
-    displayBtn.addEventListener('click', () => {
-        clearGameBoard();
-        displayGameBoard();
-        displayBtn.textContent = 'Restart Game'
-    })
-    const clearGameBoard = () => {
-        while (boardContainer.firstChild) {
-            boardContainer.removeChild(boardContainer.firstChild)
-        }
-    }
-
     const displayGameBoard = () => {
         for (i = 0; i < gameBoard.gameBoardObject.length; i++) {
             let div = document.createElement('div')
             div.dataset.boxNumber = i;
             div.classList.add('box')
             boardContainer.appendChild(div)
-
+        //adds player1 
             div.addEventListener('click', () => {
                 div.textContent = player1.getSymbol();
+                placeSpot(player1, div.dataset.boxNumber)
             })
         }
     }
+
+    let displayBtn = document.querySelector('#display-btn')
+    displayBtn.addEventListener('click', () => {
+        clearGameBoard();
+        displayGameBoard();
+        displayBtn.textContent = 'Restart Game'
+    })
+
+    const clearGameBoard = () => {
+        while (boardContainer.firstChild) {
+            boardContainer.removeChild(boardContainer.firstChild)
+        }
+    }
+    
+    const placeSpot = (player, spot) => {
+        let symbol = player.getSymbol();
+        gameBoard.gameBoardObject[spot] = symbol
+        console.log(gameBoard.gameBoardObject)
+    }
+    return {    }
 })();
